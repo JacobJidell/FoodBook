7dd9fbdf37f0481fe20a8b002bfc149c20627d3a
JacobAhlberg <jacahlberg@hotmail.com>
2017-09-09 17:17:56 +0200

    Initial Commit

---
+++ FoodBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodBook
+//
+//  Created by Jacob Ahlberg on 2017-09-08.
+//  Copyright © 2017 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
