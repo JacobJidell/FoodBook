0a9ea81c3b923307c59246cd8e9c962364a4f9fd
JacobAhlberg <jacahlberg@hotmail.com>
2017-09-11 19:32:26 +0200

    All categories images implemented

---
+++ FoodBook/Views/ShadowText.swift
@@ -0,0 +1,18 @@
+//
+//  ShadowText.swift
+//  FoodBook
+//
+//  Created by Jacob Ahlberg on 2017-09-11.
+//  Copyright © 2017 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ShadowText: UILabel {
+
+    override func awakeFromNib() {
+        
+    }
+
+}
